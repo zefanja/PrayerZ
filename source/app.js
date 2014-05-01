875e2c7eb9c4af5d81d21980eca96c5c49220e4a
zefanja <zefanja@freenet.de>
2014-05-01 21:27:21 +0700

    * initial WIP commit

--- source/app.js
+++ source/app.js
@@ -1,15 +1,90 @@
 /**
-	Define and instantiate your enyo.Application kind in this file.  Note,
-	application rendering should be deferred until DOM is ready by wrapping
-	it in a call to enyo.ready().
+    Define and instantiate your enyo.Application kind in this file.  Note,
+    application rendering should be deferred until DOM is ready by wrapping
+    it in a call to enyo.ready().
 */
-
 enyo.kind({
-	name: "myapp.Application",
-	kind: "enyo.Application",
-	view: "myapp.MainView"
+    name: 'prayerz.App',
+    kind: 'enyo.Application',
+    autoStart: false,
+    components: [
+        {kind: 'enyo.Signals', onIDBOpened: 'onIDBReady'},
+        {name: 'prayers', kind: 'enyo.Collection', model: 'pz.Prayer', url: 'prayer'},
+        {name: 'tags', kind: 'enyo.Collection', model: 'pz.Tag', url: 'tag'}
+    ],
+
+    create: enyo.inherit(function(sup) {
+        return function() {
+            var idbSource;
+            sup.apply(this, arguments);
+
+            try {
+                var idbPrayerz = new indexeddb.Source({
+                    dbName: 'prayerz',
+                    dbVersion: 1,
+                    dbInitSchema: this.initSchema.bind(this)
+                });
+
+                enyo.store.addSources({
+                    'idb': idbPrayerz
+                });
+            } catch(error) {
+                this.error('Failed to initialize IDB:', error);
+            }
+        };
+    }),
+
+    view: 'prayerz.MainView',
+    onIDBReady: function(sender, event) {
+        if (event.error) {
+            this.error('Failed to initialize IndexedDB:', event.error);
+            return true;
+        }
+
+        this.$.prayers.fetch({
+            source: 'idb',
+            success: function(rec, opts, res) {
+                this.log('Great success, starting...');
+                this.start();
+            }.bind(this),
+            fail: function(rec, opts, error) {
+                this.error(error);
+            }.bind(this)
+        });
+
+        return true;
+    },
+
+    initSchema: function(error, db, oldVer, newVer) {
+        if (error) {
+            this.error('Failed to open IndexedDB:', error);
+            return;
+        }
+
+        // Create 'prayer' store if doesn't exist
+        if (!db.objectStoreNames.contains('prayers')) {
+            db.createObjectStore('prayer', {keyPath: 'id', autoIncrement: true});
+        }
+
+        // Create 'tag' store if doesn't exist
+        if (!db.objectStoreNames.contains('prayers')) {
+            db.createObjectStore('tag', {keyPath: 'id', autoIncrement: true});
+        }
+    }
 });
 
-enyo.ready(function () {
-	new myapp.Application({name: "app"});
+enyo.kind({
+    name: 'pz.Prayer',
+    kind: 'enyo.Model',
+    url: 'prayer'
+});
+
+enyo.kind({
+    name: 'pz.Tag',
+    kind: 'enyo.Model',
+    url: 'tag'
+});
+
+enyo.ready(function() {
+    new prayerz.App().renderInto(document.body);
 });
--- source/views/views.js
+++ source/views/views.js
@@ -1,23 +1,51 @@
 /**
-	For simple applications, you might define all of your views in this file.  
-	For more complex applications, you might choose to separate these kind definitions 
-	into multiple files under this folder.
+    For simple applications, you might define all of your views in this file.
+    For more complex applications, you might choose to separate these kind definitions
+    into multiple files under this folder.
 */
 
 enyo.kind({
-	name: "myapp.MainView",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    name: "prayerz.MainView",
+    kind: "FittableRows",
+    fit: true,
+    bindings: [
+        {from: '.app.$.prayers', to: '.$.prayerList.collection'}
+    ],
+    components:[
+        {kind: "onyx.Toolbar", components: [
+            {content: "PrayerZ"},
+            {kind: "onyx.Button", content: "Tap Me", ontap: "tapped"}
+        ]},
+        {kind: "enyo.DataList", name: "prayerList", fit: true, components: [
+            {components: [
+                {name: "title"},
+                {name: "tags"}
+            ],
+            bindings: [
+                {from: ".model.title", to: ".$.title.content"},
+                {from: ".model.tags", to: ".$.tags.content"}
+            ]}
+        ]}
+    ],
+
+    tapped: function () {
+        //this.inherited(arguments);
+        //this.get(".app.$.prayers").add({title: "Mein anderes Anliegen", tags: "Allgemein, Familie"});
+        var prayer = new pz.Prayer({
+            title: "Mein anderes Anliegen",
+            tags: "Schule, Familie"
+        });
+        prayer.commit({
+            source: "idb",
+            success: function(rec, opts, res) {
+                // Now add to the collection if new
+                this.get('.app.$.prayers').add(prayer);
+
+            }.bind(this),
+            fail: function(rec, opts, res) {
+                this.error('Failed to commit a user:', res);
+            }.bind(this)
+        });
+
+    }
 });
